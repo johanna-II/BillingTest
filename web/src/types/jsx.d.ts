ffa2db8e08d7fe74af6e134d4562deb8ef43842a
johanna-II <062042@naver.com>
2025-11-06 21:10:00 +0900

    fix jsx

--- web/src/types/jsx.d.ts
+++ web/src/types/jsx.d.ts
@@ -1,28 +1,18 @@
 /**
  * Global JSX namespace declaration for React
- * Required for TypeScript to understand JSX syntax
+ * Extends React's built-in JSX types
  */
 
 import type React from 'react'
 
 declare global {
   namespace JSX {
-    // Core JSX types from React
-    interface Element extends React.ReactElement<unknown, unknown> {}
-    interface ElementClass extends React.Component<unknown> {
-      render(): React.ReactNode
-    }
-    interface ElementAttributesProperty {
-      props: Record<string, unknown>
-    }
-    interface ElementChildrenAttribute {
-      children: React.ReactNode
-    }
-
-    // Intrinsic elements (HTML tags)
+    // Use React's JSX types directly
+    interface Element extends React.JSX.Element {}
+    interface ElementClass extends React.JSX.ElementClass {}
+    interface ElementAttributesProperty extends React.JSX.ElementAttributesProperty {}
+    interface ElementChildrenAttribute extends React.JSX.ElementChildrenAttribute {}
     interface IntrinsicElements extends React.JSX.IntrinsicElements {}
-
-    // Type utilities
     interface IntrinsicAttributes extends React.JSX.IntrinsicAttributes {}
     interface IntrinsicClassAttributes<T> extends React.JSX.IntrinsicClassAttributes<T> {}
     type LibraryManagedAttributes<C, P> = React.JSX.LibraryManagedAttributes<C, P>
